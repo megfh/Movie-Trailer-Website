06540a5563dbaf0d9072ea637440b35767e725d9
megfh <megfhannon@gmail.com>
2017-03-12 13:08:45 -0400

    fixed indentation issues & added comments

--- media.py
+++ media.py
@@ -1,11 +1,19 @@
 class Movie():
-  """ A class representing a movie
-      Attributes:
+    """
+    A class representing a movie
+    Attributes:
         Title
         Poster
         Trailer URL
- """
-  def __init__(self, movie_title, movie_poster, movie_trailer):
-    self.title = movie_title
-    self.poster_image_url = movie_poster
-    self.trailer_youtube_url = movie_trailer
+    """
+
+    def __init__(self, movie_title, movie_poster, movie_trailer):
+        """
+        constructor for class Movie
+        Takes in inputs for the movie's title, poster and trailer
+        and initializes the respective instance variables with the
+        given input
+        """
+        self.title = movie_title
+        self.poster_image_url = movie_poster
+        self.trailer_youtube_url = movie_trailer
